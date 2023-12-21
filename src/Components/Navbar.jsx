80795f2985c6a9dbb997b46b40538b1cd0b90356
Istieak <istieak.ahmed@yahoo.com>
2023-12-21 23:52:33 +0600

    navbar animation

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,14 +2,32 @@ import React from "react";
 import { IoMenu } from "react-icons/io5";
 import { IoCloseSharp } from "react-icons/io5";
 import { Link } from "react-router-dom";
+import { AnimatePresence, motion } from "framer-motion";
 
 const Navbar = () => {
   const [navToggle, setNavToggle] = React.useState(false);
   const navData = [
-    {title: 'My Work', link: '/work'},
-    {title: 'My Shelf', link: '/shelf'},
-    {title: 'My Resume', link: '/resume'},
-  ]
+    { title: "My Work", link: "/work" },
+    { title: "My Shelf", link: "/shelf" },
+    { title: "My Resume", link: "/resume" },
+  ];
+  const menuAnimate = {
+    initial: {
+      scaleY: 0,
+    },
+    animate: {
+      scaleY: 1,
+      transition: {
+        duration: 0.5,
+      },
+    },
+    exit: {
+      scaleY: 0,
+      transition: {
+        duration: 0.5,
+      },
+    },
+  };
   return (
     <div>
       <div className="bg-[#4831d4] h-[90px] w-full flex items-center justify-between px-10">
@@ -28,38 +46,47 @@ const Navbar = () => {
           )}
         </div>
       </div>
-      {navToggle && (
-        <div className="h-screen w-full absolute top-0 z-40 left-0 bg-white">
-          <div className="h-[90px] flex items-center px-10">
-          <h1 className="text-[#4831d4] font-scripto text-4xl">Istieak.</h1>
-          </div>
-          <div className="px-16 py-5 flex flex-col gap-12">
-            <div className="flex flex-col gap-3 text-[#4831d4]">
-              {navData.map((item, index) => (
-                <NavItem key={index} title={item.title} link={item.link} />
-              ))}
+      <AnimatePresence>
+        {navToggle && (
+          <motion.div
+            variants={menuAnimate}
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            className="h-screen origin-top w-full absolute top-0 z-40 left-0 bg-white"
+          >
+            <div className="h-[90px] flex items-center px-10">
+              <h1 className="text-[#4831d4] font-scripto text-4xl">Istieak.</h1>
             </div>
-            <div className="flex flex-col gap-8">
+            <div className="px-16 py-5 flex flex-col gap-12">
+              <div className="flex flex-col gap-3 text-[#4831d4]">
+                {navData.map((item, index) => (
+                  <NavItem key={index} title={item.title} link={item.link} />
+                ))}
+              </div>
+              <div className="flex flex-col gap-8">
                 <p className="text-[#cbc9e2] tracking-[8px]">SAY HELLO</p>
                 <div className="flex flex-col gap-3 text-[#4831d4] font-[500]">
-                    <a href="#">hello@istieakahmed.com</a>
-                    <a href="#">+880 15711-42863</a>
+                  <a href="#">hello@istieakahmed.com</a>
+                  <a href="#">+880 15711-42863</a>
                 </div>
+              </div>
             </div>
-          </div>
-        </div>
-      )}
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
 
-const NavItem = ({title, link}) => {
+const NavItem = ({ title, link }) => {
   return (
     <div>
-      <Link className="text-xl font-[500]" to={link}>{title}</Link>
+      <Link className="text-xl font-[500]" to={link}>
+        {title}
+      </Link>
     </div>
-  )
-}
-
+  );
+};
 
 export default Navbar;
