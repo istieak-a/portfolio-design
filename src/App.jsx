33d2bc83f9520b13b54a6ad49eb143938e117ed2
Istieak <istieak.ahmed@yahoo.com>
2023-12-21 17:47:15 +0600

    Refactor code for improved performance

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from 'keep-react'
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      Welcome to istieak ahmed
+    </div>
+  )
+}
+
+export default App
