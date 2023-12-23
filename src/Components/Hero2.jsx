b3f3f7f353fb4abc8e1aea866ae23a72c08ef20f
Istieak <istieak.ahmed@yahoo.com>
2023-12-23 19:02:21 +0600

    Refactor file paths and update styling in Hero2 component

--- src/Components/Hero2.jsx
+++ src/Components/Hero2.jsx
@@ -1,8 +1,8 @@
 import { Button } from "keep-react";
 import React from "react";
-import dotsnline from '../assets/png/dotsnline.png'
-import dots from '../assets/png/dots.png'
-import dots2 from '../assets/png/5dots.png'
+import dotsnline from "../assets/png/dotsnline.png";
+import dots from "../assets/png/dots.png";
+import dots2 from "../assets/png/5dots.png";
 const Hero2 = () => {
   const serviceData = [
     {
@@ -16,24 +16,34 @@ const Hero2 = () => {
   ];
   return (
     <div className="relative">
-        <ul className="h-[90vh] px-10 md:px-20 flex flex-col justify-center md:flex-row gap-5 pt-10 [&>*:nth-child(2)]:md:pt-48 [&>*:nth-child(2)]:md:ps-44">
-      {serviceData.map((item, index) => (
-        <ServiceCard key={index} title={item.title} desc={item.desc} />
-      ))}
-    </ul>
-    <img className="h-24 hidden md:block md:absolute left-[25%] top-[66%]" src={dotsnline} />
-    <img className="h-24 hidden md:block md:absolute right-[15%] top-[20%]" src={dots} />
-    <img className="h-2 hidden md:block md:absolute right-[35%] top-[20%]" src={dots2} />
+      <ul className="flex h-[90vh] flex-col justify-center gap-5 px-10 pt-10 md:flex-row md:px-20 [&>*:nth-child(2)]:md:ps-44 [&>*:nth-child(2)]:md:pt-48">
+        {serviceData.map((item, index) => (
+          <ServiceCard key={index} title={item.title} desc={item.desc} />
+        ))}
+      </ul>
+      <img
+        className="left-[25%] top-[66%] hidden h-24 md:absolute md:block"
+        src={dotsnline}
+      />
+      <img
+        className="right-[15%] top-[20%] hidden h-24 md:absolute md:block"
+        src={dots}
+      />
+      <img
+        className="right-[35%] top-[20%] hidden h-2 md:absolute md:block"
+        src={dots2}
+      />
     </div>
   );
 };
 
-
 const ServiceCard = ({ title, desc }) => {
   return (
-    <div className=" px-5 py-5 rounded-md font-sfui">
-      <h1 className="text-4xl md:text-5xl font-black pb-5 text-[#4831d4]">{title}</h1>
-      <p className=" text-sm w-[90%] md:w-[80%] text-[#4831d4]">{desc}</p>
+    <div className=" font-sfui rounded-md px-5 py-5">
+      <h1 className="pb-5 text-4xl font-black text-[#4831d4] md:text-5xl">
+        {title}
+      </h1>
+      <p className=" w-[90%] text-sm text-[#4831d4] md:w-[80%]">{desc}</p>
     </div>
   );
 };
