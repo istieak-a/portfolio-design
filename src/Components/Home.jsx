d19e934499d34f5057fb27df353e77b958eb6af3
Istieak <istieak.ahmed@yahoo.com>
2023-12-24 13:34:19 +0600

    Refactor Home component to use a div instead of fragment

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import Hero1 from './Hero1'
-import Hero2 from './Hero2'
-import Hero3 from './Hero3'
-import Hero4 from './Hero4'
-import Hero5 from './Hero5'
+import React from "react";
+import Hero1 from "./Hero1";
+import Hero2 from "./Hero2";
+import Hero3 from "./Hero3";
+import Hero4 from "./Hero4";
+import Hero5 from "./Hero5";
 
 const Home = () => {
   return (
-    <>
-        <Hero1 />
-        <Hero2 />
-        <Hero3 />
-        <Hero4 />
-        <Hero5 />
-    </>
-  )
-}
+    <div>
+      <Hero1 />
+      <Hero2 />
+      <Hero3 />
+      <Hero4 />
+      <Hero5 />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
