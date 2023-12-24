c81be878155d9454f1bfa26b19eed48dc1e5e71f
Istieak <istieak.ahmed@yahoo.com>
2023-12-24 13:41:11 +0600

    Update import paths for Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import Hero1 from "./Hero1";
-import Hero2 from "./Hero2";
-import Hero3 from "./Hero3";
-import Hero4 from "./Hero4";
-import Hero5 from "./Hero5";
+import Hero1 from "../Components/Hero1";
+import Hero2 from "../Components/Hero2";
+import Hero3 from "../Components/Hero3";
+import Hero4 from "../Components/Hero4";
+import Hero5 from "../Components/Hero5";
 
 const Home = () => {
   return (
